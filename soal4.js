9a992a7463ec264f0028f45bf38a52fa8746d7ce
AlQasmSystem <alqasmsystem@localhost.localdomain>
2020-09-15 21:43:44 +0000

    array destruction (full)

---
+++ soal4.js
@@ -0,0 +1,10 @@
+const prabu = {
+  nama: "rizal",
+  asal: "prabumulih",
+  rest: ["smknegeriprabumulih", "prabujaya", "sukajadi"],
+};
+const { nama, asal, rest } = prabu;
+
+console.log(nama); //menampilkan 'rizal'
+console.log(asal); //menampilkan 'prabumulih'
+console.log(rest); //["smknegeriprabumulih","prabujaya","sukajadi"]
